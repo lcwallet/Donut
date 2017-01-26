5fa45534c0aa801f8e5d05e0a96e8e521ca188e6
Amiya Sahu <amiya053@gmail.com>
2017-01-26 21:27:51 +0530

    removed options framework

--- qa-theme/Donut-theme/utils/donut-options.php
+++ qa-theme/Donut-theme/utils/donut-options.php
@@ -3,11 +3,12 @@
         header( 'Location: ../../' );
         exit;
     }
-    /**
-     * This file will contain all the option names we are going to use in out theme
-     */
 
     if ( !class_exists( 'Donut_Option_Keys' ) ) {
+
+        /**
+         * Class contains the defaults options key-value pairs
+         */
         class Donut_Option_Keys
         {
             const THEME_VERSION = 'donut_theme_ver';
@@ -18,63 +19,4 @@
             const FA_CDN = '//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css';
             const BS_JS_CDN = '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js';
         }
-    }
-
-    /**
-     * Class Donut_Options
-     */
-    class Donut_Options
-    {
-        /**
-         * @var
-         */
-        protected static $instance;
-
-        protected $config;
-        protected $systemConfig;
-        protected $userConfig;
-
-        /**
-         * @return Donut_Options
-         */
-        public static function getInstance()
-        {
-            return isset( self::$instance ) ? self::$instance : self::$instance = new self();
-        }
-
-        /**
-         * Constructor function
-         */
-        final private function __construct()
-        {
-            self::init();
-        }
-
-        protected function init()
-        {
-            $this->systemConfig = require DONUT_THEME_BASE_DIR . '/utils/donut-system-defaults-options.php';
-            $this->userConfig = require DONUT_THEME_BASE_DIR . '/options/donut-user-options.php';
-
-            $this->config = array_merge( $this->systemConfig, $this->userConfig );
-        }
-
-        public function getConfig( $key )
-        {
-            return isset( $this->config[ $key ] ) ? $this->config[ $key ] : '';
-        }
-    }
-
-    /**
-     *
-     * Reads the configuration file
-     *
-     * @param $key
-     *
-     * @return string
-     *
-     * @deprecated
-     */
-    function donut_opt( $key )
-    {
-        return Donut_Options::getInstance()->getConfig( strtolower($key) );
     }
